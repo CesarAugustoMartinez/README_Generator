59efe99152a98fc9d3bb31255d9060fb86e688db
Cesar Martinez <cesaguma@hotmail.com>
2020-10-20 00:09:10 -0400

    Updating info into index.js file for README.md file

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ function init() {
     inquirer.prompt(questions)
     .then(function(response){
         writeToFile("README.md",generateReadme(response));
+        console.log("README.md file has been created!")
     })
     .catch(function(err){
         console.log(err);
